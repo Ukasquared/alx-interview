925d51fe544c4054e2df0423892fa34ffc30c056
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-27 08:32:38 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""pascal triangle"""
+
+def pascal_triangle(n):
+    """builds the pascal triangle"""
+    if type(n) is not int:
+        raise TypeError("type is not an integer")
+    if n <= 0:
+        return []
+    queue = []
+    for i in range(n):
+        if i == 0:
+            queue.append([1])
+        if i == 1:
+            queue.append([1, 1]) 
+        if i > 1:
+            prv_list_len = len(queue[i - 1])
+            in_list = [queue[i - 1][j] + queue[i - 1][j + 1]
+                        for j in range(prv_list_len - 1)]
+            in_list.insert(0, 1)
+            in_list.append(1)
+            queue.append(in_list)
+    return queue
